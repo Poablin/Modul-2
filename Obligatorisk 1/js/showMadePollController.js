1ef48dfb8aa7899042980af13c3a4489b9610f19
Poablin <kristoffer.elvejord@gmail.com>
2020-08-14 17:19:31 +0200

    Funker (Med liten bug)

--- Obligatorisk 1/js/showMadePollController.js
+++ Obligatorisk 1/js/showMadePollController.js
@@ -6,11 +6,6 @@ function createAlternativesHtml2()
 
 function addAlternative2(text) {
     model.inputs.createPoll.options.push(text)
-    showPoll()
-    updatePoll2()
-}
-
-function updatePoll2() {
     model.polls[model.app.chosenPoll].options = model.inputs.createPoll.options;
     model.polls[model.app.chosenPoll].usersCanAddAlternatives = model.inputs.createPoll.usersCanAddAlternatives;
     showPoll()
