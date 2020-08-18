fb6221a699318872be139e37e3d0284562625a99
Poablin <kristoffer.elvejord@gmail.com>
2020-08-18 13:41:56 +0200

    Fiksa showAllPollsController.js

--- Obligatorisk 1/js/showAllPollsView.js
+++ Obligatorisk 1/js/showAllPollsView.js
@@ -1,6 +1,10 @@
 function showAllPolls() {
+    let html = '';
+    for (i = 0; i < model.polls.length; i++) {
+        html += `<button onclick="selectPoll(${i})">${model.polls[i].question}</button> <br><br>`
+    }
     contentDiv.innerHTML = `
     <br>
-    ${createStoredPolls()}
+    ${html}
     `
 }
